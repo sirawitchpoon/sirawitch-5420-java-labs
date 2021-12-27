2f1977c96f31171a3710664086e7322b7a6a8357
Sirawitch <96230860+sirawitchpoon@users.noreply.github.com>
2021-12-27 17:47:18 +0700

    Add files via upload

---
+++ lab2/butryojantho/sirawitch/lab2/src/Athlete.java
@@ -0,0 +1,22 @@
+/**
+ * This Athlete program is to accept three arguments: athlete name, the sport name
+ *  that that athlete plays, and the nationality of that athlete.  Its output format is “My
+ *  favorite athlete is <athlete_name> who plays <sport_name> and has
+ * nationality as <athlete_nationality>”
+ *
+ * Author: Sirawitch Butryojantho
+ * ID: 643040542-0
+ * Sec: 1
+ * Date: December 24, 2021
+ *
+ **/
+
+public class Athlete {
+    public static void main(String[] args) {
+        String s1 = "Fernando Torres ";
+        String s2 = "Football ";
+        String s3 = "Spanish";
+        System.out.print("My favourite athlete is " + s1);
+        System.out.print("who plays " + s2 + "and has nationality as " + s3 + ".");
+    }
+}
